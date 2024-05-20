d9cda1e4dcd05d329c2c98b2719020a9e8a0367c
Jaime <jaimecaballero99@gmail.com>
2024-05-20 12:48:35 +0200

    Creación del modelo Coupon y obtenemos los datos del JSON

---
+++ src/app/pages/coupons/coupons.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Coupon} from "../../models/coupon";
+import {CouponsService} from "../../services/coupons.service";
+
+@Component({
+  selector: 'app-coupons',
+  templateUrl: './coupons.page.html',
+  styleUrls: ['./coupons.page.scss'],
+})
+export class CouponsPage implements OnInit {
+
+  public coupons: Coupon[];
+
+  constructor(
+    private couponService: CouponsService
+  ) {
+    this.coupons = [];
+  }
+
+  ngOnInit() {
+    this.couponService.getCoupons().then((coupons: Coupon[]) => {
+      this.coupons = coupons;
+      console.log(this.coupons);
+    })
+  }
+
+}
